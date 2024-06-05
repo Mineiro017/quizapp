1bf7d84e7107cb6e1e0a8436872655a6f9d2c368
Mineiro017 Moura <pedro.siqueira@eaportal.org>
2024-06-05 01:06:40 +0000

    adicionada a funçao para escolher o tema

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { trocarTema, verificarTema} from "./helpers/tema-helper"
+import { trocarTema, verificarTema} from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
@@ -8,3 +8,16 @@ botaoTema.addEventListener("click", () => {
 })
 
 verificarTema(body, botaoTema)
+
+const botoesAssuntto = document.querySelectorAll("assuntos button")
+
+botoesAssuntto.forEach()
+botoesAssuntto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
